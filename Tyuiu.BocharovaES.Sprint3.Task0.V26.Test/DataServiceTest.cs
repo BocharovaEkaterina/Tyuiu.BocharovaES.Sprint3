96dd06c4fbb7a7bf47ea19502e7d108b3bc9e1e6
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-25 01:09:03 +0500

    Add project files.

---
+++ Tyuiu.BocharovaES.Sprint3.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BocharovaES.Sprint3.Task0.V26.Lib;
+namespace Tyuiu.BocharovaES.Sprint3.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int k = 1;
+            int kl = 9;
+            double wait = -15.642;
+            double res = ds.GetMultiplySeries(k, kl);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
