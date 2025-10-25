6890aa28c72c0ac51e6e56b1c565d83aee21adbd
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-25 19:46:49 +0500

    Task1

---
+++ Tyuiu.BocharovaES.Sprint3.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BocharovaES.Sprint3.Task1.V3.Lib;
+namespace Tyuiu.BocharovaES.Sprint3.Task1.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            double wait = 24589.408;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BocharovaES.Sprint3.Task1.V3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BocharovaES.Sprint3.Task1.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Операитор цикла while                                             *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Написать программу используя цикл while, которая вычисляет произведение *");
+        Console.WriteLine("* ряда по формуле.                                                        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = 1;
+        int stopValue = 10;
+
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
