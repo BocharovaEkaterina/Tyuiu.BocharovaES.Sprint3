21c20b58a7b850f1a65479ba5aa696259ce361f3
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-26 20:36:35 +0500

    Task3

---
+++ Tyuiu.BocharovaES.Sprint3.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BocharovaES.Sprint3.Task3.V7.Lib;
+namespace Tyuiu.BocharovaES.Sprint3.Task3.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceCharOnNum()
+        {
+            DataService ds = new DataService();
+            string value = "gft hggt ntg";
+            char replaceable = 'g';
+            char replacement = '4';
+            string wait = "4ft h44t nt4";
+            string res = ds.ReplaceCharOnNum(value, replaceable, replacement);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BocharovaES.Sprint3.Task3.V7/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BocharovaES.Sprint3.Task3.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Операитор цикла foreach                                           *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Используя цикл foreach заменить буквы g на цифру 4 в строке:            *");
+        Console.WriteLine("* gft hggt ntg                                                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        string value = "gft hggt ntg";
+        char replaceable = 'g';
+        char replacement = '4';
+        string wait = "4ft h44t nt4";
+
+        Console.WriteLine("искомая сточка = " + value);
+        Console.WriteLine("заменяемое = " + replaceable);
+        Console.WriteLine("замена = " + replacement);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("новая сточка = " + ds.ReplaceCharOnNum(value, replaceable, replacement));
+        Console.ReadKey();
+    }
+}
