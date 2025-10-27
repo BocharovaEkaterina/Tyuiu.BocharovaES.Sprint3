c4e803b9ad78f4a0c73ccb42561aa8cc1eba3224
Екатерина Бочарова <kati.bocharova.06@mail.ru>
2025-10-27 14:03:26 +0500

    Task5

---
+++ Tyuiu.BocharovaES.Sprint3.Task5.V29/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.BocharovaES.Sprint3.Task5.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Бочарова Е. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнила: Бочарова Екатерина Сергеевна | ИИПб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ :                                                               *");
+        Console.WriteLine("* Написать программу, которая вычисляет сумму сумм ряда по формуле,       *");
+        Console.WriteLine("* при х = 2.                                                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 2;
+        int startValue1 = 1;
+        int stopValue1 = 3;
+        int startValue2 = 1;
+        int stopValue2 = 10;
+
+        Console.WriteLine("Переменная X = " + x);
+        Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+        Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+        Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+        Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма сумм ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+        Console.ReadKey();
+    }
+}
